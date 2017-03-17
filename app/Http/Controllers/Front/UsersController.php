ef3688f80e999199f0d88a19a7d7020204483177
LUDO\Ludovic <ludovicnay08@gmail.com>
2017-03-17 16:52:51 +0100

    Création utilisateur sans être admin

--- app/Http/Controllers/Front/UsersController.php
+++ app/Http/Controllers/Front/UsersController.php
@@ -24,7 +24,7 @@ class UsersController extends Controller
      */
     public function create()
     {
-        //
+        return view("front/users/create");
     }
     
 
--- resources/views/front/users/edit.blade.php
+++ resources/views/front/users/edit.blade.php
@@ -15,18 +15,12 @@
                 {!! BootForm::email("Email","email")->placeholder("Entrez l'email de l'utilisateur")->required(false)->defaultValue($user->email)!!}
                 {!! BootForm::password("Mot de passe","password")->placeholder("Entrez le nom de l'utilisateur")!!}
                 {!! BootForm::password("Confirmer le mot de passe","password_confirmation")->placeholder("Entrez de nouveau le mot de passe")!!}
+                {!! BootForm::select('Role', 'role')->options(['student' => 'Etudiant', 'teacher' => 'Professeur', 'admin' => 'Administrateur'])->select('student') !!}
             </div>
         </div>
     </div>
     <div class="col-md-4 col-md-push-2">
-        <div class="box-header box-primary">
-            <div class="box-header with-border">
-                <h2 class="box-title">Date de Naissance</h2>
-            </div>
-            <div class="box-body">
-                {!! BootForm::date('Date de Naissance',"birthday")->defualtValue($user->birthday) !!}
-            </div>
-        </div>
+
         <div class="box-body">
             <input class="btn btn-primary pull-right" type="submit" value="Modifier"/>
             {!! BootForm::close() !!}
