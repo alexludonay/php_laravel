d04ac8af54ba181142dece53aea9559fefc5fd97
Michel Bouchou <michel.bouchou@gmail.com>
2017-03-21 16:04:02 +0100

    Commentaire Pt.2

--- resources/views/front/profils/show.blade.php
+++ resources/views/front/profils/show.blade.php
@@ -1,4 +1,3 @@
-@extends("front/default")
 <body>
 <div class="profile-title">
     @if (Auth::user()->id == $user->id)
@@ -22,5 +21,42 @@
         <p>Description : Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
 </div>
+@if (Auth::guest())
+    <p>Veuillez vous connecter pour pouvoir laisser un commentaire</p>
+@else
+
+
+
+    <div class="box-body col-md-6">
+        {!! BootForm::open()->action(route("commentaires.store")) !!}
+        {!! BootForm::textarea("Commentaire","content")->placeholder("Votre commentaire : ") !!}
+        {!! BootForm::hidden("auteur")->value(Auth::user()->nom) !!}
+        {!! BootForm::hidden("user_id")->value($user->id) !!}
+
+
+        <input class="btn btn-primary pull-right" type="submit" value="Commenter"/>
+        {!! BootForm::close() !!}
+
+    </div>
+
+
+@endif
+
+@foreach($user->commentaires as $commentaire)
+    {{$commentaire->auteur}} - {{$commentaire->content}}
+
+    <br />
+
+    @if (!Auth::guest())
+        @if(Auth::user()->nom==$commentaire->auteur)
+            <a href="{{route("commentaires.edit", $commentaire)}}" ><input class="" type="submit" value="Modifier"/></a>
+            {!! BootForm::open()->action(route("commentaires.destroy", $commentaire))->style("display:inline") !!}
+            {!! BootForm::hidden("_method")->value("delete") !!}
+            <input class="" type="submit" value="Supprimer"/>
+            {!! BootForm::close() !!}
+            <br />
+        @endif
+    @endif
+@endforeach
 </body>
 </html>
