99be4efd5ae430429663bed07cc3c4bee5037ce2
LUDO\Ludovic <ludovicnay08@gmail.com>
2017-03-28 12:37:24 +0200

    test

--- resources/views/front/commun/header.blade.php
+++ resources/views/front/commun/header.blade.php
@@ -14,13 +14,17 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a style="color: #eceff1;" href="#" class="hov">Administrateurs</a></li>
-                    <li><a style="color: #eceff1;" href="#" class="hov">Profil</a></li>
-                    <li><a style="color: #eceff1;"href="#" class="hov">Les classements</a></li>
+                    @if (Auth::check() and Auth::user()->role=='admin')
+                        <li><a style="color: #eceff1;" href="{!! route("admin.users.index") !!}" class="hov">Administrateurs</a></li>
+                    @endif
                     @if (Auth::check())
-                        <li><a style="color: #eceff1;" href="{!! route("deconnexion") !!}" class="bordure">Se deconnecter</a></li>
+                        <li><a style="color: #eceff1;" href="{!! route("profil.show", Auth::user()->id) !!}" class="hov">Profil</a></li>
+                    @endif
+                    <li><a style="color: #ffffff;" href="{!! route("notes") !!}">Les classements</a></li>
+                    @if (Auth::check())
+                        <li><a  style="color: #ffffff;" href="{!! route("deconnexion") !!}" class="bordure">Se deconnecter</a></li>
                     @else
-                        <li><a style="color: #eceff1;" href="{!! route("login") !!}" class="bordure">Se connecter</a></li>
+                        <li><a style="color: #ffffff;" href="{!! route("login") !!}" class="bordure">Se connecter</a></li>
                     @endif
                 </ul>
             </div>
