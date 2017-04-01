91b0cba9de621d5f209345f0eddff202ecde5f57
LUDO\Ludovic <ludovicnay08@gmail.com>
2017-04-01 18:34:01 +0200

    Ajout de différent boutton pour acceder a toutes les interfaces !

--- resources/views/front/profils/index.blade.php
+++ resources/views/front/profils/index.blade.php
@@ -1,10 +1,23 @@
 @include('default')
+
+<div class="col-sm-6 col-sm-offset-3 text-center rechercher">
+    <form method="POST" id="form_recherche" action="{{route('search')}}">
+        <div class="form-group">
+            <input type="hidden" name= "_token" value="{{csrf_token()}}"/>
+            <label for="rechercher" class="sr-only">Rechercher :</label>
+            <input type="text" name="query" class="form-control" id="espace_recherche" placeholder="Qui recherchez-vous ?" required="">
+        </div>
+        <button type="submit" id="bouton-rechercher"><i class="fa fa-search visible-xs" aria-hidden="true"></i><span class="hidden-xs">Rechercher</span></button>
+    </form>
+<h1 class="titre_accueil text-center"> Retrouvez tous les candidats : </h1>
+    </div>
 @foreach($users as $user)
     <div class="col-md-4 col-sm-12 text-center profil-utilisateurs">
         <div class="image_profil center-block"></div>
         <a href="{!! route("profil.show", $user->id) !!}">{{ $user->prenom }}</a>
         <h4>{{ $user->nom }}</h4>
         <p>Note : {{ $user->note }}</p>
+            <a href="{{ route("profil.show", $user->id)}}"><input class="btn btn-success center-block" type="submit" value="En savoir plus"/></a>
     </div>
 
 @endforeach
--- resources/views/front/users/index.blade.php
+++ resources/views/front/users/index.blade.php
@@ -1,11 +1,15 @@
 @include('default')
 
+
 <h4 class="col-sm-12 text-center titre_accueil">Rechercher un candidat :</h4>
 <div class="text-center">
 {!! Form::open(['route' => 'admin.search']) !!}
 {!! Form::text('query') !!}
 {!! Form::submit('Rechercher') !!}
 {!! Form::close() !!}
+    @if (Auth::user()->role =="admin")
+        <a href="{{ route("admin.users.create")}}"><input class="btn btn-primary center-block" type="submit" style="margin-top: 15px;" value="Créer un utilisateur"/></a>
+    @endif
 </div>
 @foreach($users as $user)
     <div class="col-md-6 col-sm-12 col-lg-4  text-center profil-utilisateurs">
@@ -15,7 +19,7 @@
         <p>Note : {{ $user->note }}</p>
         <a href="{!! route("profil.show", $user->id) !!}"><input class="btn btn-default center-block" type="submit" value="Voir le Profil"/></a>
             <a href="{{ route("admin.users.edit",$user)}}"><input class="btn btn-primary" type="submit" value="Modifier"/></a>
-            <a href="{{ route("admin.users.destroy",$user)}}"> <input type="submit" value="Supprimer" class="del btn btn-danger"/></a>
+            <a href="{{ route("admin.users.destroy",$user->id)}}"><input type="submit" value="Supprimer" class="del btn btn-danger"/></a>
 
         </div>
 
